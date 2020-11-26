4181ca69ea96b1fa45b364deb4b8e05da622f48c
Bikram Thapa <robertmagar456@gmail.com>
2020-11-25 23:07:19 -0600

    added comments

--- src/Assignment3/Fahrenheitdemo.java
+++ src/Assignment3/Fahrenheitdemo.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Fahrenheitdemo {
 
 	public static void main(String[] args) {
-		// f to  Celsius
+		// f to  Celsius , we want to know more about celsius
 		
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter fahrenheit: ");
